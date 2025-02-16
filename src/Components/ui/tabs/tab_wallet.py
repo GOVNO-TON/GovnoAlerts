1d43d496a2c554d5fc99b8c859bddc532b5c7f3b
cathome1 <cathome.bro@yandex.ru>
2025-02-17 01:29:01 +0300

    small fix to remove unintentional spaces

--- src/Components/ui/tabs/tab_wallet.py
+++ src/Components/ui/tabs/tab_wallet.py
@@ -22,7 +22,7 @@ class WalletTab:
     def save_wallet_api_key(self):
         text = str(self.ui.wallet_api_key_textbox.toPlainText())
         if text != "":
-            self.data_wallet["api_key"] = text
+            self.data_wallet["api_key"] = text.replace(' ', '')
             self.save_json(self.JSON_FILE_WALLET, {"wallet_settings": self.data_wallet})
             self.obs_wallet = Wallet(self.data_wallet["wallet_raw"], self.data_constants["jetton_master"], self.data_wallet["api_key"], self.data_constants["api_url"], db_path=self.path(self.LOCAL_PATH, self.data_constants["db_path"]), update_ui=self.update_table_view)
             QMessageBox.information(self, "Успех", "Настройки сохранены!")
@@ -33,7 +33,7 @@ class WalletTab:
     def save_wallet_raw_adr(self):
         text = str(self.ui.wallet_addr_textbox.toPlainText())
         if text != "":
-            self.data_wallet["wallet_raw"] = text
+            self.data_wallet["wallet_raw"] = text.replace(' ', '')
             self.save_json(self.JSON_FILE_WALLET, {"wallet_settings": self.data_wallet})
             self.obs_wallet = Wallet(self.data_wallet["wallet_raw"], self.data_constants["jetton_master"], self.data_wallet["api_key"], self.data_constants["api_url"], db_path=self.path(self.LOCAL_PATH, self.data_constants["db_path"]), update_ui=self.update_table_view)
             QMessageBox.information(self, "Успех", "Настройки сохранены!")
